67713c044d0280f1de5f5ff7e5f8230fa6255c08
Cristopher G <cristopher.gamboa.c@gmail.com>
2025-11-10 21:42:25 -0300

    Update default success url

--- src/main/java/com/kume/kume/configuration/config/SecurityConfig.java
+++ src/main/java/com/kume/kume/configuration/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/auth/login").permitAll()
-                        .defaultSuccessUrl("/recipes", true)
+                        .defaultSuccessUrl("/recipes/list", true)
                         .failureUrl("/auth/login?error=true"))
                 .logout(logout -> logout
                         .permitAll()
